2a71943c8bf9e8bdda491041e9634fef7fd4c061
yfujii01 <yfujii01@gmail.com>
2020-11-27 22:56:03 +0900

    一通り動いた

---
+++ src/main/java/com/example/demo/entity/Book.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+
+import java.util.List;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class Book {
+  /** ID */
+  private Long id;
+
+  /** タイトル */
+  @NotNull @NotEmpty private String title;
+
+  /** 国際標準図書番号 */
+  @NotNull private String isbn;
+
+  /** 公開年 */
+  @NotNull private String publicationYear;
+
+  /** 出版社ID */
+  private Long pubId;
+
+  /** (関連)出版社情報 */
+  private Publisher publisher;
+
+  /** (関連)著者情報 */
+  private List<Author> authors;
+}
